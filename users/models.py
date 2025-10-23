20811550e75f789ea541db9115d120f5e75176cc
IanCamilo26 <ian777.com@Gmail.com>
2025-10-23 00:35:40 -0300

    Creación y modificación de archivos, tanto en la carpeta del proyecto como en la de la app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=30, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} <{self.email}>"
